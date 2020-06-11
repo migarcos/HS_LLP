5262a342fcb5ee10c1f42897459f5ff9c5fd46cf
Miguel <migark@hotmail.com>
2020-06-11 02:47:13 +0000

    trying C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-include <stdio.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * A code to display  a phrase using put
  */
+
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
