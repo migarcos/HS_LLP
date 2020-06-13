4e9a7a5a7d652a87f0151981c047ea905f71ddec
Miguel <migark@hotmail.com>
2020-06-13 19:56:41 +0000

    snd C day

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: intenger value
+ */
+int main(void)
+{
+	char m;
+	char l;
+
+	for (m = '0'; m <= '9'; m++)
+		putchar(m);
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: integer value
+ */
+int main(void)
+{
+	int m;
+
+	m = '0';
+	while (m <= '9')
+	{
+		putchar(m);
+		if (m != '9')
+		{
+			putchar(44);
+			putchar(32);
+		}
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
