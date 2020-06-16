d1e78740095a43b772a484047b36d7d26b727096
Miguel <migark@hotmail.com>
2020-06-16 17:05:20 +0000

    funcloops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,13 +7,21 @@
  */
 int _abs(int n)
 {
-	if (n > 0)
-	{
-		return (n);
-	}
-	else
-	{
+	if (n < 0)
 		n = n * -1;
-		return (n);
-	}
+
+	return (n);
 }
+
+/*
+ *   if (n > 0)
+ *	{
+ *		return (n);
+ *	}
+ *	else
+ *	 {
+ *		n = n * -1;
+ *		return (n);
+ *	}
+ */
+
