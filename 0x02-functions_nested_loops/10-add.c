f85bc1a73b56f74a3bb06ee74ffc0095b862404f
Miguel <migark@hotmail.com>
2020-06-16 22:34:30 +0000

    funtloops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @x: fst value
+ * @y: snd value
+ *
+ * Return: integer
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: initial number
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d, ", n);
+
+		if (n < 98)
+			n++;
+		else
+			n--;
+	}
+
+	printf("98 \n");
+}
