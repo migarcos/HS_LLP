869c073882010a6019541a3e42ee04bf1ec2a6ba
Miguel <migark@hotmail.com>
2020-06-18 21:09:45 +0000

    funcloops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,12 @@ void more_numbers(void)
 	{
 		int i;
 
-		for (i = 0; i <= 14; i++)	
+		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
 			_putchar((i / 10) + '0');
 
-			_putchar((i % 10) + '0');			
+			_putchar((i % 10) + '0');
 		}
 
 		_putchar('\n');
