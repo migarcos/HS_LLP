9c8bca23d9fa92f7e29be2fdf862d7671f33ed85
Miguel <migark@hotmail.com>
2020-06-18 21:12:44 +0000

    funcloops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _isupper: checks for uppercase character
+ * _isupper - checks for uppercase character
  * @c: value to check
  *
  * Return: 1 if c is uppercase, 0 otherwise
