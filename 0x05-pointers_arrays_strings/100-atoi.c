5f9a7c791d2c7e77af09ada669e5515170679ce6
Miguel <migark@hotmail.com>
2020-06-26 01:59:38 +0000

    pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *  _atoi - convert a string to an integer
- *  @s : string to reception
+ * _atoi - convert a string to an integer
+ * @s : string to reception
  *
- *   Return - int
+ * Return - int
  */
 int _atoi(char *s)
 {
 	int i = 0, j = 0, num = 0, sign = 1;
+
 	do { 
 		if (s[i] == '-')
 			sign = sign * -1;
@@ -20,7 +21,7 @@ int _atoi(char *s)
 		}
 		else if (j > 0  &&  s[i] < '0' && s[i] > '9')
 			break;
-	}while (s[i] != '\0');
+	} while (s[i] != '\0');
 
 	num = num * sign;
 
