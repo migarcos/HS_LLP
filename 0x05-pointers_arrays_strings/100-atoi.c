eb29e7e1e5242d5d5d45a92afd5040f869f9b769
Miguel <migark@hotmail.com>
2020-06-26 01:32:01 +0000

    pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,8 +8,11 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, j = 0, num = 0;
+	int i = 0, j = 0, num = 0, sign = 1;
 	do { 
+		if (s[i] == '-')
+			sign = sign * -1;
+
 		if (s[i]>'0' && '9' < s[i])
 		{
 			num = (num * 10) + (s[i] - '0');
@@ -18,6 +21,8 @@ int _atoi(char *s)
 		else if (j > 0  &&  s[i] < '0' && s[i] > '9')
 			break;
 	}while (s[i] != '\0');
+
+	num = num * sign;
 
 	if (j > 0 )
 		return (num);
