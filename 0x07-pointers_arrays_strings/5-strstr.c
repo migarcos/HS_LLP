31c33b280378151176f5f523ac154fbf78f356b4
Miguel <migark@hotmail.com>
2020-07-01 11:51:18 +0000

    pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strstr -  finds the first occurrence of the substring
+ * @s: original string
+ * @accept: string to verify ocurrence
+ * Return: a pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *o = haystack, *s = needle;
+
+		while (*haystack && *needle && *haystack == *s)
+		{
+			haystack++;
+			s++;
+		}
+		if (!*s)
+			return (o);
+
+		haystack = o + 1;
+	}
+	return ('\0');
+}
