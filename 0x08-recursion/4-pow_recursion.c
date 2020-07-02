f0a927a4068a912d984a6908d853964917f605a2
Miguel <migark@hotmail.com>
2020-07-02 04:47:28 +0000

    recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion -  returns x value raised to pwr y
+ * @x: the number
+ * @y: potency
+ * Return: int
+ **/
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
+
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - square root of a number
+ * @n: the number
+ * 
+ * Return: int
+ **/
+int auxsqrt(int n, int i)
+{
+	if (n == (i * i))
+		return (i);
+	else if (n > (i * i))
+             return (auxsqrt(n , i + 1));
+	else
+		return (-1);
+}
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+
+	return (auxsqrt(n, i + 1));
+}
+
