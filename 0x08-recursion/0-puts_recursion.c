5acfcc7a222a4e47a78c6e322e0baea55cc6ac5b
Miguel <migark@hotmail.com>
2020-07-02 02:50:02 +0000

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to print
+ *
+ * Return: void
+ **/
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\n');
+}
+
