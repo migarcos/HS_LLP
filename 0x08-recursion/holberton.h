27a23a9d3941470ff3addddeb8805916659943ba
Miguel <migark@hotmail.com>
2020-07-03 03:42:07 +0000

    recursion

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -11,4 +11,6 @@ int auxsqrt(int n, int i);
 int is_prime_number(int n);
 int aux_is_prime(int x, int y);
 int is_palindrome(char *s);
+int _strlen_recursion(char *s);
+int helper_palindrome(char *s, int len);
 int wildcmp(char *s1, char *s2);
