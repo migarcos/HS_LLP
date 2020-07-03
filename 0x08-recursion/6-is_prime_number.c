0b834dcfc6efc40da3038afbb68a59e6e36f3771
Miguel <migark@hotmail.com>
2020-07-03 19:56:52 +0000

    fix comments

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,13 +4,19 @@
  * is_prime_number - look for a prime number
  * @n: intger input
  *
- * Retur: 1 if the input is prime
+ * Return: 1 if the input is prime
  ***/
 int is_prime_number(int n)
 {
 	return (aux_is_prime(n, 2));
 }
 
+/**
+ * aux_is_prime - the recursion function
+ * @x: number
+ * @y: ctrlVar
+ * Return: 1 is prime, 0 to NO
+ **/
 int aux_is_prime(int x, int y)
 {
 	if (x <= 1)
