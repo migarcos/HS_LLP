200b633d4a0ae0ea01127b508b3e84e947ab296a
Miguel <migark@hotmail.com>
2020-07-07 01:48:33 +0000

    commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: argument counter
+ * @argv: argument with values
+ *
+ * Return: integer
+ ***/
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	while (*argv)
+	{
+		printf("%s\n", *argv);
+		argv++;
+	}
+		
+	return (0);
+}
