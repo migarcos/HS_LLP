ddd65e589b46b21af3a56f1da9555f6eab1f5f6b
Miguel <migark@hotmail.com>
2020-07-07 01:31:16 +0000

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints qty args 
+ * @argc: argument counter
+ * @argv: argument with values
+ *
+ * Return: integer
+ ***/
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
