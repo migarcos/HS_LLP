e43a729432b72d4db6ad1ccf489ce10a08031c04
Miguel <migark@hotmail.com>
2020-07-07 02:05:57 +0000

    commiit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: argument counter
+ * @argv: argument with values
+ *
+ * Return: integer
+ ***/
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	if (argc < 3) 
+	{
+		printf("ERROR \n");
+		return (1);
+	}
+	else 
+		printf("%d \n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
