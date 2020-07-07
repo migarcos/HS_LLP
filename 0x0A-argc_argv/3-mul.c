20879aeff97fc25663a8fa012a0786bfdefa346b
Miguel <migark@hotmail.com>
2020-07-07 02:13:10 +0000

    commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,14 +7,14 @@
  *
  * Return: integer
  ***/
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	if (argc < 3) 
+	if (argc < 3)
 	{
 		printf("ERROR \n");
 		return (1);
 	}
-	else 
+	else
 		printf("%d \n", atoi(argv[1]) * atoi(argv[2]));
 
 	return (0);
