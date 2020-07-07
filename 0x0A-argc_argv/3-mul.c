9d06623810e16ea7a521293b935b188d68467495
Miguel <migark@hotmail.com>
2020-07-07 03:08:06 +0000

    commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,15 +9,19 @@
  ***/
 int main(int argc, char *argv[])
 {
+	int mult;
+
 	if (argc != 3)
 	{
 		printf("ERROR \n");
 		return (1);
 	}
+	else
+	{
+		mult = atoi(argv[1]) * atoi(argv[2]);
 
-	int mult = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d \n", mult);
 
-	printf("%d \n", mult);
-
-	return (0);
+		return (0);
+	}
 }
