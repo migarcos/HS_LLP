6ca486451f6152bfed4fb68e9e9431997566d764
Miguel <migark@hotmail.com>
2020-07-09 04:44:10 +0000

    malloc

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -10,11 +10,34 @@
 
 char *argstostr(int ac, char **av)
 {
-	*ptr;
+	char *ptr;
+	int i, j, k = 0, lenght = 0;
 
 	if (av == NULL || ac == NULL)
 		return(NULL);
 
-	return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			length++;
+		}
+	}
+
+	ptr = malloc((length + 1 + ac) * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			ptr[k] == av[i][j];
+
+		ptr[k++] = '\n';
+	}
+	ptr[k] = '\0';
+
+	return (ptr);
 
 }
