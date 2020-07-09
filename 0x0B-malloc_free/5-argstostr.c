20df8dc596c1a3b1d1594bd7cfae7226f2814a7f
Miguel <migark@hotmail.com>
2020-07-09 04:15:22 +0000

    malloc

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: size
+ * @av: char matrix
+ * Return: pointer to a matrix
+ */
+
+char *argstostr(int ac, char **av)
+{
+	*ptr;
+
+	if (!char || ac == NULL)
+		return(NULL);
+
+	return (NULL);
+
+}
