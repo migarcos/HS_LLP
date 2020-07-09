8767225e21d85aba74013adfacdf18f074d8e1cd
Miguel <migark@hotmail.com>
2020-07-09 04:58:34 +0000

    malloc

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,20 +1,52 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
 
 /**
- * strtow - splits a string into words.
+ * strtow - splits strings to words
  * @str: string to split
- *
- * Return: array of strings.
+ * Return: pointer to an array of words
  */
 
 char **strtow(char *str)
 {
-	char **result = NULL;
+	char **array;
+	int i, j, count, len, k, m;
 
-	if (str == NULL)
+	count = k = 0;
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
-
-
-	return (result);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			count++;
+	}
+	if (count == 0)
+		return (NULL);
+	array = malloc(((count + 1) * sizeof(char *)));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; str[i] !=  '\0' && k < count; i++)
+	{
+		if (str[i] != ' ')
+		{
+			len = 0;
+			j = i;
+			while (str[j] != ' ' && str[j] != '\0')
+				j++, len++;
+			array[k] = malloc((len + 1) * sizeof(char));
+			if (array[k] == NULL)
+			{
+				for (k = k - 1; k >= 0; k++)
+					free(array[k]);
+				free(array);
+				return (NULL);
+			}
+			for (m = 0; m < len; m++, i++)
+				array[k][m] = str[i];
+			array[k++][m] = '\0';
+		}
+	}
+	array[k] = NULL;
+	return (array);
 }
