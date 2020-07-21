c58b64a201c5454f3cfd7df6689dd9cbe5c84ec1
Miguel <migark@hotmail.com>
2020-07-21 17:48:42 +0000

    Ox0D

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the name of file
+ * @void: not arguments
+ *
+ * Return: 0 to ready
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
